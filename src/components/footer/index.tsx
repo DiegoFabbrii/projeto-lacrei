3b305d0ea6353cbd12edd5c141c561b52538f37e
DiegoFabbrii <diegofabbri@outlook.com>
2023-05-27 23:52:43 -0300

    feat: add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import * as S from "./style";
+import { Link } from "gatsby";
+
+import { StaticImage } from "gatsby-plugin-image";
+
+export const Footer = () => {
+  return (
+    <S.Container>
+      <S.FooterList>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/pessoa-usuario">Pessoa Usuário</Link>
+        </li>
+
+        <li>
+          <Link to="/profissional">Profissional</Link>
+        </li>
+      </S.FooterList>
+      <S.SocialMedias>
+        <S.SocialMediaItem href="https://www.facebook.com/lacrei.saude">
+          <StaticImage
+            src="../../assets/facebook-logo.svg"
+            alt="facebook logo"
+          />
+        </S.SocialMediaItem>
+
+        <S.SocialMediaItem href="https://www.instagram.com/lacrei.saude/">
+          <StaticImage
+            src="../../assets/instagram-logo.svg"
+            alt="instagram logo"
+          />
+        </S.SocialMediaItem>
+
+        <S.SocialMediaItem href="https://www.linkedin.com/company/lacrei">
+          <StaticImage
+            src="../../assets/linkedin-logo.svg"
+            alt="linkedin logo"
+          />
+        </S.SocialMediaItem>
+      </S.SocialMedias>
+    </S.Container>
+  );
+};
---
+++ src/components/footer/style.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  max-width: 1238px;
+  width: 90vw;
+
+  margin: 0 auto;
+`;
+
+export const FooterList = styled.ul`
+  display: flex;
+  gap: 40px;
+
+  margin-top: 32px;
+
+  a {
+    color: #1f1f1f;
+
+    :hover {
+      filter: brightness(170%);
+    }
+  }
+
+  [aria-current="page"] {
+    font-weight: 700;
+  }
+
+  @media only screen and (max-width: 520px) {
+    flex-direction: column;
+    gap: 5px;
+
+    font-size: 0.875rem;
+  }
+`;
+
+export const SocialMedias = styled.div`
+  display: flex;
+  gap: 40px;
+
+  margin: 28px 0;
+`;
+
+export const SocialMediaItem = styled.a`
+  transition: 0.2s;
+  :hover {
+    filter: brightness(130%);
+  }
+`;
