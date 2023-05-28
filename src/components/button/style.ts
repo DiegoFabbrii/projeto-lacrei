c142b922884187dfdca773b930cf7304b6ce67a8
DiegoFabbrii <diegofabbri@outlook.com>
2023-05-28 14:23:46 -0300

    feat: update button css

--- src/components/button/style.ts
+++ src/components/button/style.ts
@@ -12,7 +12,7 @@ export const Button = styled.button<ButtonStyleProps>`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   border-radius: 8px;
-  border: ${({ borderColor }) => `2px solid ${borderColor}` || "none"};
+  border: ${({ borderColor }) => `2px solid ${borderColor}`};
 
   cursor: pointer;
 
