b73b4b86b87646d2c2eca8400a530c7388baf642
DiegoFabbrii <diegofabbri@outlook.com>
2023-05-29 15:58:18 -0300

    feat: add links data

---
+++ src/data/links.ts
@@ -0,0 +1,21 @@
+interface Link {
+  label: string;
+  link: string;
+}
+
+export const links: Link[] = [
+  {
+    label: "Home",
+    link: "/",
+  },
+
+  {
+    label: "Pessoa Usuária",
+    link: "/pessoa-usuaria",
+  },
+
+  {
+    label: "Profissional",
+    link: "/pessoa-profissional",
+  },
+];
